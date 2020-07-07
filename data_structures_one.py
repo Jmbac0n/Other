8f31d0f7a4a94b19410ba5da05595739c26c6ddb
Jmbac0n <jakematthias@hotmail.com>
2020-07-07 22:28:46 +0100

    Playing with data structures

---
+++ data_structures_one.py
@@ -0,0 +1,27 @@
+# Lists
+
+num_list = [1, 2, 3, 4, 5]
+print(num_list)
+
+num_list.append(6)
+print(num_list)
+
+num_list.insert(4, 7)
+print(num_list)
+
+num_list.remove(7)
+print(num_list)
+
+# Tuple
+
+a_tuple = 1, 'a', 2.0, 'b'
+print(a_tuple)
+
+# Dictionary
+
+simple_dict = {'a': 1, 'b': 2, 'c': 3}
+
+# Use Cases for later projects
+
+# Saving files into accessible list for loading in terminal
+# Same but for loading encryption keys
